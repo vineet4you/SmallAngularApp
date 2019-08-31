1d670a0054d163e5651de56761bf5504c0587aab
Vineet <techvineet>
2019-09-01 02:00:36 +0530

    My second commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,29 @@
+import { Component } from '@angular/core';
+
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+
+
+export class AppComponent {
+//title="myFirstApp";
+items=["Vineet","Sumit","Banke"];
+newItem="";
+pushItem= function()
+{
+  if(this.newItem !="")
+  {
+    this.items.push(this.newItem);
+    this.newItem="";
+  }
+}
+
+removeItem=function(index)
+{
+  this.items.splice(index,1);
+}
+
+}
